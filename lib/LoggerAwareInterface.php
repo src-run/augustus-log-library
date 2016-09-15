aa6f9ab615a6edd20f272690de72475bdb07c9a1
Rob Frawley 2nd <rmf@src.run>
2016-09-14 20:13:52 -0400

    added logger aware interface to complement trait

---
+++ lib/LoggerAwareInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the `src-run/augustus-log-library` project.
+ *
+ * (c) Rob Frawley 2nd <rmf@src.run>
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace SR\Log;
+
+use Psr\Log\LoggerAwareInterface as BaseLoggerAwareInterface;
+
+/**
+ * Interface for logger aware.
+ */
+interface LoggerAwareInterface extends BaseLoggerAwareInterface
+{
+}
+
+/* EOF */
